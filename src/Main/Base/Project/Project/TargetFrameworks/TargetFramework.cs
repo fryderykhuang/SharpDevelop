c5ad75a6d59dc649ae508853d203f351b3531b4e
Xelwon <xelwonpgo@gmail.com>
2017-07-25 17:58:42 +0200

    fixed things for dotnet 46

--- src/Main/Base/Project/Project/TargetFrameworks/TargetFramework.cs
+++ src/Main/Base/Project/Project/TargetFrameworks/TargetFramework.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2014 AlphaSierraPapa for the SharpDevelop Team
+// Copyright (c) 2014 AlphaSierraPapa for the SharpDevelop Team
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this
 // software and associated documentation files (the "Software"), to deal in the Software
@@ -35,6 +35,7 @@ namespace ICSharpCode.SharpDevelop.Project
 		public static readonly TargetFramework Net35Client = new DotNet35Client();
 		public static readonly TargetFramework Net40 = new DotNet4x(Versions.V4_0, RedistLists.Net40, DotnetDetection.IsDotnet40Installed);
 		public static readonly TargetFramework Net40Client = new DotNet4xClient(Versions.V4_0, RedistLists.Net40Client, DotnetDetection.IsDotnet40Installed);
+		public static readonly TargetFramework Net46 = new DotNet4x(Versions.V4_6, RedistLists.Net45, DotnetDetection.IsDotnet45Installed);
 		public static readonly TargetFramework Net45 = new DotNet4x(Versions.V4_5, RedistLists.Net45, DotnetDetection.IsDotnet45Installed);
 		public static readonly TargetFramework Net451 = new DotNet4x(Versions.V4_5_1, RedistLists.Net45, DotnetDetection.IsDotnet451Installed);
 		public static readonly TargetFramework Net452 = new DotNet4x(Versions.V4_5_2, RedistLists.Net45, DotnetDetection.IsDotnet452Installed);
